242df78a5db8dff32dcaa1eac3f7a8597e22ac3f
hyebinchoi <144509096+hyebinchoi@users.noreply.github.com>
2024-07-03 03:30:06 +0900

    Title: 특정 조건 세 정수 비교, Time: 131ms, Memory: 25MB, Status: Passed - CodeTree

--- 240703/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
+++ 240703/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -3,11 +3,11 @@ a = int(a)
 b = int(b)
 c = int(c)
 
-if a <= b and b <= c :
+if a <= b and a <= c :
     print(1, end=" ")
 else :
     print(0, end=" ")
 if a == b and b == c :
     print(1)
 else : 
-   print(0)
+    print(0)
