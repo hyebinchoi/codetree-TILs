f188309864883a954c81dc9ffe9ca0805ef85c5c
hyebinchoi <144509096+hyebinchoi@users.noreply.github.com>
2024-07-04 19:43:04 +0900

    Title: 최댓값 구하기, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240704/최댓값 구하기/maximum-value.py
+++ 240704/최댓값 구하기/maximum-value.py
@@ -4,9 +4,9 @@ a = int(a)
 b = int(b)
 c = int(c)
 
-if (a > b) and (a > c) :
+if (a >= b) and (a >= c) :
     print(a)
-elif (b > a ) and (b >c) :
+elif (b >= a ) and (b >=c) :
     print(b)
 else :
     print(c)
