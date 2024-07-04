e15f93d94997c3e79adeacf1794a06f29bbbcf6f
hyebinchoi <144509096+hyebinchoi@users.noreply.github.com>
2024-07-04 18:07:06 +0900

    Title: 좀 더 어려운 수학 점수, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240704/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,14 @@
+a,b = input().split()
+c,d = input().split()
+
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+
+if a>c :
+    print("A")
+elif (b == d) and (a > c) :
+    print("A")
+else :
+    print("B")
