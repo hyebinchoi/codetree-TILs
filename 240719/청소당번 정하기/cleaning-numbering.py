3b541fd29130da393eade50ace8dbbe4f43c5a4f
hyebinchoi <144509096+hyebinchoi@users.noreply.github.com>
2024-07-19 04:42:33 +0900

    Title: 청소당번 정하기, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

--- 240719/청소당번 정하기/cleaning-numbering.py
+++ 240719/청소당번 정하기/cleaning-numbering.py
@@ -3,11 +3,11 @@ ent2 = 0
 ent3 = 0
 ent12 = 0
 
-for i in range(0,n):
-    if i>=2 and i %2 == 0:
-        ent2 += 1
-    if i>=3 and i %3 == 0 :
+for i in range(1,n+1):
+    if i % 12 ==0 :
+        ent12 += 1  
+    elif i %3 == 0 :
         ent3 += 1
-    if i>=12 and i %12 ==0 :
-        ent12 += 1
+    elif i %2 == 0: 
+        ent2 += 1
 print(ent2, ent3, ent12)
